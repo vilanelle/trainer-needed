6d62f914cd172adabe36c35d9183723fd5e9e1c5
Piotr <p_sajdak@o2.pl>
2019-07-28 15:18:26 +0200

    Issues notifications sent

--- public/client.js
+++ public/client.js
@@ -10,15 +10,48 @@ document.addEventListener('DOMContentLoaded', () => {
   const getNodeById = id => document.getElementById(id);
   const renderLandingView = () => {
     renderTemplateById('landing');
+      
+    const trainerParticipant = getNodeById('loginTrainer');
+    trainerParticipant.addEventListener('click', renderTrainerLoginView);
+    
+    const loginParticipant = getNodeById('loginParticipant');
+    loginParticipant.addEventListener('click', renderParticipantLoginView)
+
   };
   const renderParticipantLoginView = () => {
     renderTemplateById('participantLogin');
+    
+    const participantLoginForm = getNodeById('participantLoginForm');
+      participantLoginForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const formData = new FormData(event.target);
+        const name = formData.get('name');
+        const group = formData.get('group');
+        sendEvent('PARTICIPANT_LOGIN', {name, group});
+      });
+
   };
   const renderTrainerLoginView = () => {
     renderTemplateById('trainerLogin');
+    const trainerLoginForm = getNodeById('trainerLoginForm');
+    trainerLoginForm.addEventListener('submit', e => {
+      
+      e.preventDefault();
+      const formData = new FormData(event.target);
+      const name = formData.get('name');
+      sendEvent('TRAINER_LOGIN', {name});
+    });
+
   };
   const renderIssueSubmitView = () => {
     renderTemplateById('issueSubmit');
+    const issueSubmitForm = getNodeById('issueSubmitForm');
+    issueSubmitForm.addEventListener('submit', e => {
+      e.preventDefault();
+      const formData = new FormData(event.target);
+      const problem = formData.get('problem');
+      sendEvent('TRAINER_NEEDED', { problem });
+    });
   };
   const renderIssueReceivedView = () => {
     renderTemplateById('issueReceived');
@@ -30,8 +63,62 @@ document.addEventListener('DOMContentLoaded', () => {
     renderTemplateById('hintReceived');
   };
   const renderTrainerDashboardView = data => {
+    console.log('data', data)
     renderTemplateById('trainerDashboard');
+
+    const issueListItemTemplate = getNodeById('issueListItem');
+    const issueListNode = getNodeById('issueList');
+    data.forEach(it => {
+      const issueListItemNode = document.importNode(issueListItemTemplate.content, true);
+      issueListItemNode.querySelector('.issueListItemName').textContent = it.userName;
+      issueListItemNode.querySelector('.issueListItemGroup').textContent = it.userGroup;
+      issueListItemNode.querySelector('.issueListItemProblem').textContent = it.problem;
+      issueListItemNode.querySelector('.issueListItemStatus').textContent = it.status;
+      issueListNode.appendChild(issueListItemNode);
+    });
+
   };
 
   renderLandingView();
+  const socket = new WebSocket('ws://localhost:5000');
+  
+  socket.onopen = e => {
+    console.log('onopen', e);
+  };
+
+  socket.onclose = event => {
+    console.log(['WebSocket.onclose'], event);
+  };
+
+  socket.onmessage = event => {
+    const { action, payload } = JSON.parse(event.data);
+    switch (action) {
+      case 'PARTICIPANT_LOGGED': {
+        renderIssueSubmitView();
+        break;
+      }
+      case 'ISSUE_RECEIVED': {
+        renderIssueReceivedView();
+        break;
+      }
+      case 'ISSUES': {
+        renderTrainerDashboardView(payload);
+        break;
+      }
+    }
+  };
+
+  socket.onerror = event => {
+    console.error(['WebSocket.onerror'], event);
+  };
+
+  const sendEvent = (action, payload) => {
+    try {
+      socket.send(JSON.stringify({ action, payload }));
+    }
+  
+    catch (e) {
+      console.error(e);
+    }
+  };
 });
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import {
   StaticFileExtension,
   User,
   Event,
+  ContentType,
+  Issue,
 } from './types';
 
 const PORT = process.env.PORT || 5000;
@@ -18,4 +20,96 @@ const FILE_EXTENSION_TO_CONTENT_TYPE: FileExtensionToContentTypeMap = {
   html: 'text/html',
   ico: 'image/x-icon',
   js: 'text/javascript',
+};
+
+const server = http.createServer(function(req, res) {
+    try {
+      const url = req.url === '/' ? 'index.html' : req.url;
+      const urlParts = url ? url.split('.') : '';
+      const fileExtension = urlParts[urlParts.length - 1];
+      const contentType = FILE_EXTENSION_TO_CONTENT_TYPE[fileExtension];
+      
+      res.writeHead(200,{ 'Content-Type': contentType });
+      const file = readFileSync(`${process.cwd()}/public/${url}`);
+      res.end(file);
+    }
+    catch(e) {
+      console.log(e)
+      res.end(e);
+    }
+  
+}).listen(PORT, function() {
+  console.log("Server running on PORT: " + PORT)
+});
+
+
+const webSocketsServer = new WebSocket.Server({ server: server });
+webSocketsServer.on('connection', (socket: WebSocket) => {
+  const connectedUser: User = {
+    id: `user-id-${Date.now()}`,
+    data: {
+      name: '',
+      group: '',
+    },
+    socket,
+  };
+
+  socket.on('message', (data) => {
+    const { action, payload } = JSON.parse(data.toString());
+    switch (action as Action) {
+      case 'PARTICIPANT_LOGIN': {
+        connectedUser.data.name = payload.name;
+        connectedUser.data.group = payload.group;
+        state.participants.push(connectedUser);
+        sendEvent(socket, { action: 'PARTICIPANT_LOGGED' });
+        break;
+      }
+      case 'TRAINER_LOGIN': {
+        connectedUser.data.name = payload.name;
+        state.trainers.push(connectedUser);
+        sendEvent(socket, { action: 'TRAINER_LOGGED' });
+        break;
+      }
+      case 'TRAINER_NEEDED': {
+
+        const issue: Issue = {
+          id: '',
+          status: 'PENDING',
+          userId: '',
+          userName: '',
+          userGroup: '',
+          problem: '',
+        };
+      
+        issue.id = Date.now().toString();
+        issue.status = 'PENDING';
+        issue.userId = connectedUser.id;
+        issue.userName = connectedUser.data.name;
+        issue.userGroup = connectedUser.data.group;
+        state.issues.push(issue);
+        sendEvent(socket, { action: 'ISSUE_RECEIVED' });
+        state.trainers.forEach(trainer => sendEvent(trainer.socket, { action: 'ISSUES', payload: state.issues }));
+        break;
+      }
+      default: {
+        console.error('unknown action');
+      }
+    }
+  });
+});
+
+
+const state: State = {
+  participants: [],
+  trainers: [],
+  issues: [],
+};
+
+const sendEvent = (socket: WebSocket, event: Event): void => {
+  try {
+    socket.send(JSON.stringify(event));
+  }
+  catch (e) {
+    console.error(e);
+  }
 };
